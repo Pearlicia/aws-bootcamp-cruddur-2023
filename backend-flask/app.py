92f473cf76b0b512921b3d3fbef5ad2769f39926
Felicia Ebikon <feliciaebikon@gmail.com>
2023-07-13 17:20:48 +0000

    change rollbar init code

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -94,23 +94,42 @@ def after_request(response):
   LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
   return response
 
+
 # Rollbar ----------
 rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
-@app.before_first_request
-def init_rollbar():
-    """init rollbar module"""
-    rollbar.init(
-        # access token
-        rollbar_access_token,
-        # environment name
-        'production',
-        # server root directory, makes tracebacks prettier
-        root=os.path.dirname(os.path.realpath(__file__)),
-        # flask already sets up logging
-        allow_logging_basic_config=False)
+# @app._got_first_request
+# def init_rollbar(rollbar_access_token):
+#     if rollbar_access_token:
 
-    # send exceptions from `app` to rollbar, using flask's signal system.
-    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+#       """init rollbar module"""
+#       rollbar.init(
+#           # access token
+#           rollbar_access_token,
+#           # environment name
+#           'production',
+#           # server root directory, makes tracebacks prettier
+#           root=os.path.dirname(os.path.realpath(__file__)),
+#           # flask already sets up logging
+#           allow_logging_basic_config=False)
+
+#       # send exceptions from `app` to rollbar, using flask's signal system.
+#       got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+def init_rollbar(rollbar_access_token):
+    if rollbar_access_token:
+        # Initialize Rollbar
+        rollbar.init(
+            # access token
+            rollbar_access_token,
+            # environment name
+            'production',
+            # server root directory, makes tracebacks prettier
+            root=os.path.dirname(os.path.realpath(__file__)),
+            # flask already sets up logging
+            allow_logging_basic_config=False)
+
+        # send exceptions from `app` to rollbar, using flask's signal system.
+        got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 @app.route('/rollbar/test')
 def rollbar_test():
@@ -205,4 +224,4 @@ def data_activities_reply(activity_uuid):
   return
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(debug=True, port=4567)
